471b0c9e81e108a17398f480051c53c516933f45
astrostl <astrostl@users.noreply.github.com>
2025-10-18 11:57:14 -0500

    Update Homebrew formula for v1.2.1 with correct SHA256 checksums

--- Formula/slack-butler.rb
+++ Formula/slack-butler.rb
@@ -6,10 +6,10 @@ class SlackButler < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/astrostl/slack-butler/releases/download/v1.2.1/slack-butler-v1.2.1-darwin-arm64.tar.gz"
-    sha256 "PLACEHOLDER_ARM64_SHA256"
+    sha256 "1f0f0adaa0c297d1abff7393eea7fd4aea89603da576640c2e570c2a1e622954"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/astrostl/slack-butler/releases/download/v1.2.1/slack-butler-v1.2.1-darwin-amd64.tar.gz"
-    sha256 "PLACEHOLDER_AMD64_SHA256"
+    sha256 "57653ab2d816f5fd612fe23df10ddddafa81d7f38be1662be345b94f9cd14100"
   else
     odie "slack-butler is only supported on macOS via Homebrew. Use 'go install' or build from source for other platforms."
   end
